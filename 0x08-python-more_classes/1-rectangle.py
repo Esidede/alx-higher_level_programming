36a269b45f771194ef952a94c04e0625056ccb84
Esidede <audreymapfearon@gmail.com>
2023-04-05 17:34:48 -0700

    ang

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+class Square:
+    """Represent a square."""
+
+    def _int_(self, size):
+        """Initailize a new Square.
+        Args:
+        size (int): The size of a new square.
+        """
+        self._size = size
